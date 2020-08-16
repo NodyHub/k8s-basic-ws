0db5b64bc933fa33b0f92141d600adb6a55156eb
NodyHub <github@janharrie.de>
2020-08-16 09:56:29 +0200

    app rework

--- 00_prep/k8s-ws/app/app.py
+++ 00_prep/k8s-ws/app/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    SECRET_LOG = os.getenv('SECRET_LOG')
-    with open(SECRET_LOG) as f:
-        content = "".join(f.readlines())
+    # Get DB connection string
+    DB_CON_STRING = os.getenv('DB_CON_STRING')
+
+    # Create directory listing
+    DATA_DIR = os.getenv('DATA_DIR')
+    dir_listing = "\n".join([ "%s/%s" % (DATA_DIR, x) for x in os.listdir(DATA_DIR)])
     
-    return 'Content of "%s":\n%s' % (SECRET_LOG, content)
+    return 'DB connection string: %s\nDirectory listing for "%s":\n%s' % (DB_CON_STRING, DATA_DIR, dir_listing)
